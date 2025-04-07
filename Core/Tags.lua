5fdeed6654001bbc00f93a2cbf3cde875fc8a245
Dale Hunt <dalehunt@gmail.com>
2025-04-07 22:04:40 +0100

    Swap back to `-` for health

--- Core/Tags.lua
+++ Core/Tags.lua
@@ -13,7 +13,7 @@ oUF.Tags.Methods["Health:CurHPwithPerHP"] = function(unit)
     if unitStatus then
         return unitStatus
     else
-        return string.format("%s | %.1f%%", UUF:FormatLargeNumber(unitHealth), unitHealthPercent)
+        return string.format("%s - %.1f%%", UUF:FormatLargeNumber(unitHealth), unitHealthPercent)
     end
 end
 
